b51e291745afe4b4bd9395387b921a9ba2c9438a
Purple Myst <PurpleMyst@users.noreply.github.com>
2018-04-08 17:22:51 +0200

    Add colored text

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,24 @@
 #![feature(exact_chunks)]
+
+extern crate colored;
+use colored::Colorize;
+
 mod vm;
 
 fn main() {
     let mut vm = vm::VM::new();
     vm.load_program_from_file("challenge.bin").unwrap();
 
-    while vm.cycle().unwrap() {}
+    loop {
+        match vm.cycle() {
+            Ok(true) => {},
+
+            Ok(false) => break,
+
+            Err(msg) => {
+                eprintln!("{}", msg.red());
+                break;
+            },
+        }
+    }
 }
