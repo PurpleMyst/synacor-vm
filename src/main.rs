46168988103651c4ddb2a157201f838e5a2010ae
PurpleMyst <PurpleMyst@users.noreply.github.com>
2021-01-06 17:24:54 +0100

    refactor: break widgets into functions

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,8 @@ use std::{
     io::{self, Seek, Write},
 };
 
-use crossterm::event::KeyCode;
+use crossterm::{event::KeyCode, write_ansi_code};
 use eyre::{bail, Result};
-
-use crossterm::write_ansi_code;
 
 use tui::{
     layout::*,
@@ -17,8 +15,6 @@ use tui::{
 };
 
 type VM = synacor_vm::VM<io::Cursor<Vec<u8>>, io::Cursor<Vec<u8>>>;
-
-const NOTABLE_ADDRESSES: &[u32] = &[3952];
 
 fn run_until_prompt(vm: &mut VM, writes: &mut Vec<(u32, u32)>) -> Result<()> {
     let pos = usize::try_from(vm.output.position())?;
@@ -47,6 +43,60 @@ fn run_until_prompt(vm: &mut VM, writes: &mut Vec<(u32, u32)>) -> Result<()> {
     Ok(())
 }
 
+fn make_output_widget(vm: &VM) -> Paragraph {
+    Paragraph::new(
+        std::str::from_utf8(&vm.output.get_ref()[vm.output.position() as usize..])
+            .unwrap()
+            .trim(),
+    )
+    .block(Block::default().borders(Borders::ALL).title("Output"))
+    .wrap(Wrap { trim: true })
+}
+
+fn make_writes_widget(writes: &io::Cursor<Vec<(u32, u32)>>) -> List {
+    List::new(
+        writes
+            .get_ref()
+            .iter()
+            .skip(writes.position() as usize)
+            .map(|(dest, src)| {
+                ListItem::new(Spans::from(vec![
+                    Span::from(format!("{:5}", dest)),
+                    Span::from(" <- "),
+                    Span::from(format!("{}", src)),
+                ]))
+            })
+            .collect::<Vec<_>>(),
+    )
+    .block(Block::default().borders(Borders::ALL).title("Writes"))
+}
+
+fn make_state_widget(vm: &VM) -> Table {
+    let mut rows = vec![Row::new(vec![
+        Cell::from("pc").style(Style::default().add_modifier(Modifier::BOLD)),
+        Cell::from(vm.pc.to_string()),
+    ])];
+
+    for (idx, register) in vm.registers.iter().enumerate() {
+        rows.push(Row::new(vec![
+            Cell::from(format!("r{}", idx)).style(Style::default().add_modifier(Modifier::BOLD)),
+            Cell::from(register.to_string()),
+        ]))
+    }
+
+    Table::new(rows)
+        .block(Block::default().borders(Borders::ALL).title("State"))
+        .widths(&[Constraint::Percentage(50), Constraint::Percentage(50)])
+}
+
+fn make_prompt_widget(vm: &VM) -> Paragraph {
+    Paragraph::new(
+        std::str::from_utf8(&vm.input.get_ref()[vm.input.position() as usize..]).unwrap(),
+    )
+    .block(Block::default().borders(Borders::ALL).title("Input"))
+    .wrap(Wrap { trim: true })
+}
+
 fn main() -> Result<()> {
     color_eyre::install()?;
 
@@ -64,13 +114,15 @@ fn main() -> Result<()> {
         )
     });
 
-    // Terminal initialization
+    // Initialize our tui::Terminal
     let mut terminal = {
         let stdout = io::stdout();
         let backend = tui::backend::CrosstermBackend::new(stdout);
         tui::Terminal::new(backend)?
     };
 
+    // Set the terminal into raw mode and go into the alternate screen so we
+    // don't mess up the scrollback. Also, on exit, return the terminal into its normal state.
     crossterm::terminal::enable_raw_mode()?;
     crossterm::execute!(io::stdout(), crossterm::terminal::EnterAlternateScreen)?;
     scopeguard::defer! {
@@ -82,87 +134,25 @@ fn main() -> Result<()> {
 
     loop {
         terminal.draw(|frame| {
-            let regions = Layout::default()
+            let output_n_input = Layout::default()
                 .direction(Direction::Vertical)
                 .constraints([Constraint::Percentage(90), Constraint::Length(1)].as_ref())
                 .split(frame.size());
 
-            let output = {
-                Paragraph::new(
-                    std::str::from_utf8(&vm.output.get_ref()[vm.output.position() as usize..])
-                        .unwrap()
-                        .trim(),
-                )
-                .block(Block::default().borders(Borders::ALL).title("Output"))
-                .wrap(Wrap { trim: true })
-            };
-
-            let mut rows = vec![Row::new(vec![
-                Cell::from("pc").style(Style::default().add_modifier(Modifier::BOLD)),
-                Cell::from(vm.pc.to_string()),
-            ])];
-
-            for (idx, register) in vm.registers.iter().enumerate() {
-                rows.push(Row::new(vec![
-                    Cell::from(format!("r{}", idx))
-                        .style(Style::default().add_modifier(Modifier::BOLD)),
-                    Cell::from(register.to_string()),
-                ]))
-            }
-
-            let state = Table::new(rows)
-                .block(Block::default().borders(Borders::ALL).title("State"))
-                .widths(&[Constraint::Percentage(50), Constraint::Percentage(50)]);
-
-            let writes_w = List::new(
-                writes
-                    .get_ref()
-                    .iter()
-                    .skip(writes.position() as usize)
-                    .map(|(dest, src)| {
-                        let is_notable = NOTABLE_ADDRESSES.binary_search(&dest).is_ok();
-
-                        let mut dest = Span::from(format!("{:5}", dest));
-
-                        if is_notable {
-                            dest.style = dest.style.fg(Color::LightRed);
-                        }
-
-                        ListItem::new(Spans::from(vec![
-                            dest,
-                            Span::from(" <- "),
-                            Span::from(format!("{}", src)),
-                        ]))
-                    })
-                    .collect::<Vec<_>>(),
-            )
-            .block(Block::default().borders(Borders::ALL).title("Writes"));
-
-            let chunks = Layout::default()
+            let output_n_debug = Layout::default()
                 .direction(Direction::Horizontal)
                 .constraints([Constraint::Percentage(85), Constraint::Percentage(25)])
-                .split(regions[0]);
+                .split(output_n_input[0]);
 
-            let chunks2 = Layout::default()
+            let state_n_writes = Layout::default()
                 .direction(Direction::Vertical)
                 .constraints([Constraint::Min(11), Constraint::Percentage(100)])
-                .split(chunks[1]);
+                .split(output_n_debug[1]);
 
-            frame.render_widget(output, chunks[0]);
-
-            frame.render_widget(state, chunks2[0]);
-            frame.render_widget(writes_w, chunks2[1]);
-
-            // Render the prompt at the end
-            let prompt = {
-                Paragraph::new(
-                    std::str::from_utf8(&vm.input.get_ref()[vm.input.position() as usize..])
-                        .unwrap(),
-                )
-                .block(Block::default().borders(Borders::ALL).title("Input"))
-                .wrap(Wrap { trim: true })
-            };
-            frame.render_widget(prompt, regions[1]);
+            frame.render_widget(make_output_widget(&vm), output_n_debug[0]);
+            frame.render_widget(make_state_widget(&vm), state_n_writes[0]);
+            frame.render_widget(make_writes_widget(&writes), state_n_writes[1]);
+            frame.render_widget(make_prompt_widget(&vm), output_n_input[1]);
         })?;
 
         match crossterm::event::read()? {
@@ -180,10 +170,6 @@ fn main() -> Result<()> {
                     writes.set_position(0);
                     run_until_prompt(&mut vm, writes.get_mut())?;
                     vm.input.seek(io::SeekFrom::End(0))?;
-
-                    writes
-                        .get_mut()
-                        .sort_by_key(|&(dest, _)| NOTABLE_ADDRESSES.binary_search(&dest).is_err());
                 }
 
                 KeyCode::Char(ch) => vm.append_input(&[ch as u8])?,
